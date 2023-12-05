b4ad78b5a5b6ef11dae5f1465a591ea3daf57f7e
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-05 16:53:48 +0300

    Header video

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import './App.css';
-import styled from 'styled-components';
+import './App.css'
+import {Header} from './layout/header/Header'
 
 
 function App() {
     return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
+        <div>
+            <Header/>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Logo} from '../../components/logo/Logo'
+import {Menu} from '../../components/menu/Menu'
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo/>
+            <Menu/>
+        </StyledHeader>
+    )
+}
+
+const StyledHeader = styled.header`
+  background-color: #6d966b;
+  display: flex;
+  justify-content: space-between;
+`
