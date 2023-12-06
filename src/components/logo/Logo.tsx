b4f0a4dab9d88862ebacb413a84c92581f124d9d
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-06 17:05:57 +0300

    favicon commit3

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import {Icon} from '../icon/Icon'
 export const Logo = () => {
     return (
         <a href="#">
-            <Icon iconId={'logo'} width={'800px'} height={'800px'} viewBox={'0 0 800 800'}/>
+            <Icon iconId={'logo'}/>
         </a>
     )
 }
