f9aa54f6395fd61830e1a94c63f3f985c2d2c38c
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-07 15:15:34 +0300

    animation

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -16,3 +16,5 @@ export const Icon = (props: IconPropsType) => {
 }
 
 
+
+
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Icon} from '../icon/Icon'
+import styled from 'styled-components'
+import {LogoAnimation} from '../../styles/animations/Animations'
 
 export const Logo = () => {
     return (
-        <a href="#">
+        <StyledLogoIcon href="#">
             <Icon iconId={'logo'} width={'120'} height={'120'} viewBox={'0 0 32 32'}/>
-        </a>
+        </StyledLogoIcon>
     )
 }
 
+const StyledLogoIcon = styled.a`
+  animation: ${LogoAnimation} 30s linear infinite;
+`
 
