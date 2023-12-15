6580953fa9aea0702138f4adb991704f4a70d00b
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-15 10:31:26 +0300

    menu

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -9,15 +9,7 @@ export const HeaderMenu = (props: { menuItems: Array<string> }) => {
                 {props.menuItems.map((el: string, index: number) => {
                     return (
                         <ListItem key={index} role="menuitem">
-                            <Link href="#">
-                                {el}
-                                <Mask>
-                                    <span>{el}</span>
-                                </Mask>
-                                <Mask>
-                                    <span>{el}</span>
-                                </Mask>
-                            </Link>
+                            <Link href="#">{el}<span></span></Link>
                         </ListItem>)
                 })}
             </ul>
@@ -28,69 +20,50 @@ export const HeaderMenu = (props: { menuItems: Array<string> }) => {
 const StyledHeaderMenu = styled.nav`
   ul {
     display: flex;
-    gap: 30px;
+    gap: 20px;
     justify-content: center;
   }
 `
 
 const Link = styled.a`
+  position: relative;
+
   font-family: 'DM Sans', sans-serif;
   font-size: 20px;
   font-weight: 500;
   text-align: center;
-  color: transparent;
-`
-
-const Mask = styled.span`
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: inline-block;
-  height: 50%;
-  overflow-y: hidden;
   color: ${theme.colors.primaryTextColor};
-
-  & + & {
-    top: 50%;
-
-    span {
-      display: inline-block;
-      transform: translateY(-50%);
-    }
-  }
-`
-
-const ListItem = styled.li`
-  position: relative;
-
-  &::before {
-    content: '';
-    display: inline-block;
-    height: 3px;
-    background-color: ${theme.colors.accent};
-
-    position: absolute;
-    top: 50%;
-    left: -10px;
-    right: -10px;
-    z-index: 1;
-
-    transform: scale(0);
-  }
+  padding: 6px 20px;
+  transition: .5s;
+  z-index: 2;
 
   &:hover {
-
-    &::before {
-      transform: scale(1);
-    }
-
-    ${Mask} {
-      transform: skewX(12deg) translateX(5px);
-      color: ${theme.colors.accent};
-
-      & + ${Mask} {
-        transform: skewX(12deg) translateX(-5px);
-      }
-    }
+    color: ${theme.colors.accent};
+    //background-color: darkorange;
   }
+
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    border-bottom: 2px solid ${theme.colors.accent};
+    border-radius: 15px;
+    transform: scale(0) translateY(30px);
+    opacity: 0;
+    transition: .5s;
+
+  }
+
+  &:hover span {
+    transform: scale(1) translateY(0);
+    opacity: 1;
+  }
+`
+
+
+const ListItem = styled.li`
+
 `
