8976851844b7947d75f43289b6f793e0e5ccd3d6
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-17 09:46:19 +0300

    slogan_3 - 1

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -6,7 +6,6 @@ import {FlexWrapper} from '../../../components/FlexWrapper'
 import {font} from '../../../styles/Common'
 
 
-
 export const Slogan = () => {
     return (
         <StyledSlogan>
@@ -24,29 +23,46 @@ const StyledSlogan = styled.section`
 `
 
 const StyledTitle = styled.p`
-  text-align: center;
   font-family: 'DM Sans', sans-serif;
   font-size: 58px;
   font-weight: 700;
-  letter-spacing: -1px;
   line-height: 70px;
   color: ${theme.colors.primaryTitle};
+  text-align: center;
+  letter-spacing: -1px;
+
+  @media ${theme.media.tablet} {
+    ${font({family: '\'DM Sans\', sans-serif', weight: 700, color: theme.colors.primaryTitle, lineHeight: 1.2, fontD: 58, fontM: 30})};
+  }
 `
 
 const StyledEmail = styled.a`
-  ${font({family: 'DM Sans, sans-serif', lineHeight: 1.2, weight: 700, color: theme.colors.primaryTextColor, fontD: 58, fontM: 20})};
-  //font-family: 'DM Sans', sans-serif;
-  //font-size: 58px;
-  //font-weight: 700;
-  //line-height: 70px;
+  font-family: 'DM Sans', sans-serif;
+  font-size: 58px;
+  font-weight: 700;
+  line-height: 70px;
   letter-spacing: -1px;
-    // color: ${theme.colors.primaryTextColor};
+  color: ${theme.colors.primaryTextColor};
 
   &:hover {
     background-image: linear-gradient(90deg, ${theme.colors.accent}, ${theme.colors.contactColor});
-    //color: transparent;
-    ${font({lineHeight: 1.2, weight: 700, color: 'transparent', fontD: 60, fontM: 25})};
-    //font-size: 60px;
+    color: transparent;
+    font-size: 60px;
+    -webkit-background-clip: text;
+  }
+
+  @media ${theme.media.tablet} {
+    background-image: linear-gradient(90deg, ${theme.colors.accent}, ${theme.colors.contactColor});
+    color: transparent;
+    font-size: 40px;
+    -webkit-background-clip: text;
+  }
+
+
+  @media ${theme.media.mobile} {
+    background-image: linear-gradient(90deg, ${theme.colors.accent}, ${theme.colors.contactColor});
+    color: transparent;
+    font-size: 25px;
     -webkit-background-clip: text;
   }
 `
