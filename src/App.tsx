fb628e86bf44e07b867efabbe11b7c791f8bd351
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-20 16:53:58 +0300

    delete hobby

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {Header} from './layout/header/Header'
 import {Main} from './layout/sections/main/Main'
 import {Skills} from './layout/sections/skills/Skills'
 import {Works} from './layout/sections/works/Works'
-import {Hobbies} from './layout/sections/hobbies/Hobbies'
 import {Contact} from './layout/sections/contacts/Contact'
 import {Footer} from './layout/footer/Footer'
 import {About} from './layout/sections/about/About'
@@ -17,7 +16,6 @@ function App() {
             <About/>
             <Skills/>
             <Works/>
-            <Hobbies/>
             <Contact/>
             <Footer/>
         </div>
