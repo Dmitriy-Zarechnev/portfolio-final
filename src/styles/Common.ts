a11983fa1416f9d744c9197e3de486cc91cff5c1
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-26 13:46:13 +0300

    26.12 - fontfinish

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -1,30 +1,9 @@
-import {theme} from './theme'
-
-type FontPropsType = {
-    family?: string
-    weight?: number
-    color?: string
-    lineHeight?: number
-    fontM?: number
-    fontD?: number
-}
-
-
-export const font = ({family, weight, color, lineHeight, fontM, fontD}: FontPropsType) => `
-    font-family:${family || 'Poppins'};
-    font-weight:${weight || 400};
-    color:${color || theme.colors.primaryTextColor};
-    line-height:${lineHeight || 1.4};
-    font-size:calc( (100vw - 360px)/(1600 - 360) * (${fontD} - ${fontM}) + ${fontM}px);
- `
-
 type respFontType = {
     PxMax: number
     PxMin: number
     Vmax?: number
     Vmin?: number
 }
-
 
 export const respFont = ({PxMax, PxMin, Vmax, Vmin}: respFontType) => `
 font-size: ${respFontFnc({PxMax, PxMin, Vmax, Vmin})}
