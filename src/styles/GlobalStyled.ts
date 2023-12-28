ffab305e7f033a939660f96954d0573e9d832abc
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-28 18:55:23 +0300

    28.12-1

--- src/styles/GlobalStyled.ts
+++ src/styles/GlobalStyled.ts
@@ -14,7 +14,15 @@ export const GlobalStyle = createGlobalStyle`
       animation: none;
     }
   }
-
+ // Стили для автокомплита
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active{
+    -webkit-box-shadow: 0 0 0 30px ${theme.colors.buttonBg} inset !important;
+    color: ${theme.colors.accent};
+  }
+  
   // Стили при выделении
   *::selection {
     color: ${theme.colors.contactColor};
